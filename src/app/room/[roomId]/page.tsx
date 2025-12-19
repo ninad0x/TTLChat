cc2fadc8b211a718d57fc0f0037eb649924c7df2
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-19 23:49:13 +0530

    chat page update

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+
+function formatTimeRemaining(seconds: number) {
+    const mins = Math.floor(seconds / 60)
+    const secs = seconds % 60
+    return `${mins}:${secs.toString().padStart(2, "0")}`
+}
 
 export default function Page() {
 
     const params = useParams()
     const roomId = params.roomId as string
 	const [ copyStatus, setCopyStatus ] = useState("COPY")
+    const [ timeRemaining, setTimeRemaining ] = useState<number | null>(121)
+    const [ input, setInput ] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null)
 
 	const copyLink = () => {
 		const url = window.location.href
@@ -25,7 +35,45 @@ export default function Page() {
                         <button onClick={copyLink} className="text-[10px] bg-zinc-800 hover:bg-zinc-700 px-2 py-0.5 rounded hover:text-zinc-200 transition-colors">{copyStatus}</button>
                     </div>
                 </div>
+                
+                <div className="h-9 w-px bg-zinc-800"/>
+
+                <div className="flex flex-col">
+                    <span className="text-sm text-zinc-500 uppercase">Self-Destruct</span>
+                    <span className={`text-sm font-bold flex items-center gap-2 ${
+                        timeRemaining !== null && timeRemaining < 60 
+                        ? "text-red-500"
+                        : "text-amber-500"
+                    }`}>{timeRemaining !== null
+                        ? formatTimeRemaining(timeRemaining)
+                        : "--:--"
+                    }</span>
+                </div>
             </div>
+            <button className="text-xs bg-zinc-800 hover:bg-red-600 hover:text-white px-3 py-1.5 rounded text-zinc-400 font-bold transition-all group flex items-center gap-2 disabled:opacity-50"><span className="group-hover:animate-pulse">💣</span>DESTROY NOW</button>
         </header>
+
+        <div className="flex-1 overflow-auto p-4 space-y-4 scrollbar-thin">messages {input}</div>
+
+        <div className="flex gap-3 p-4 border-t border-zinc-800 bg-zinc-900/30">
+            <div className="flex-1 relative group">
+                <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">{">"}</span>
+                <input 
+                    ref={inputRef}
+                    value={input}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter" && input.trim()) {
+                            // TODO: send msg to backend
+                            inputRef.current?.focus()
+                        }
+                    }}
+                    placeholder="Type message..."
+                    onChange={(e) => setInput(e.target.value)}
+                    autoFocus 
+                    type="text" 
+                    className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder::text-zinc-700 py-3 pl-8 pr-4 text-sm" />
+            </div>
+            <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer">SEND</button>
+        </div>
     </main> 
 }
