062c5f69f55ebddb549f46884e4d5c957235c130
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-20 23:01:06 +0530

    feat: add message route

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,10 +1,13 @@
 import { redis } from '@/lib/redis'
 import { Elysia } from 'elysia'
 import { nanoid } from 'nanoid'
+import { authMiddleware } from './auth'
+import z from "zod"
+import { Message, realtime } from '@/lib/realtime'
 
 const ROOM_TTL_SECONDS = 60 * 10
 
-const rooms = new Elysia({ prefix: "/room"})
+const rooms = new Elysia({ prefix: "/room" })
     .post("/create", async () => {
         const roomId = nanoid()
 
@@ -17,12 +20,44 @@ const rooms = new Elysia({ prefix: "/room"})
 
         return { roomId }
     })
-    
+
+const messages = new Elysia({ prefix: "/messages" })
+    .use(authMiddleware)
+    .post("/", async ({ body, auth }) => {
+        const { sender, text } = body
+        const { roomId } = auth
+
+        const roomExists = await redis.exists(`meta:${roomId}`)
+
+        if (!roomExists) {
+            throw new Error("Room does not exist")
+        }
+
+        const message: Message = {
+            id: nanoid(),
+            sender,
+            text,
+            timestamp: Date.now(),
+            roomId
+        }
+
+        await redis.rpush(`messages:${roomId}`, 
+            { ...message, token: auth.token })
+
+        await realtime.channel(roomId).emit("chat.message", message)
 
 
 
+    }, {
+        body: z.object({
+            sender: z.string().max(100),
+            text: z.string().max(1000),
+        })
+    })
 
-const app = new Elysia({ prefix: "/api" }).use(rooms)
+
+
+const app = new Elysia({ prefix: "/api" }).use(rooms).use(messages)
 
 
 export const GET = app.fetch 
