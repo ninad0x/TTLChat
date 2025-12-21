0c29c7ab6276a16326d4d6d351ac0e4273875d87
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-21 23:32:47 +0530

    app updated

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -20,6 +20,27 @@ const rooms = new Elysia({ prefix: "/room" })
 
         return { roomId }
     })
+    .use(authMiddleware)
+    .get("/ttl", async ({ auth }) => {
+
+        const ttl = await redis.ttl(`meta:${auth.roomId}`)
+        return { ttl: ttl > 0 ? ttl : 0}
+
+    }, { query: z.object({ roomId: z.string() }) })
+    .delete("/", async ({ auth }) => {
+        
+        await realtime.channel(auth.roomId).emit("chat.destroy", { isDestroyed: true })
+
+        await Promise.all([
+            redis.del(auth.roomId),
+            redis.del(`meta:${auth.roomId}`),
+            redis.del(`messages:${auth.roomId}`)
+        ])
+
+
+    }, { query: z.object({ roomId: z.string() })})
+
+
 
 const messages = new Elysia({ prefix: "/messages" })
     .use(authMiddleware)
@@ -46,6 +67,13 @@ const messages = new Elysia({ prefix: "/messages" })
 
         await realtime.channel(roomId).emit("chat.message", message)
 
+        const remaining = await redis.ttl(`meta:${roomId}`)
+
+        await Promise.all([
+            redis.expire(`messages:${roomId}`, remaining),
+            redis.expire(`history:${roomId}`, remaining),
+            redis.expire(roomId, remaining)
+        ])
 
 
     }, {
@@ -54,13 +82,26 @@ const messages = new Elysia({ prefix: "/messages" })
             text: z.string().max(1000),
         })
     })
+    .get("/", async ({ auth }) => {
+        const messages = await redis.lrange<Message>(`messages:${auth.roomId}`, 0, -1)
+
+        return {
+            messages: messages.map((m) => ({
+                ...m,
+                token: m.token === auth.token 
+                    ? auth.token
+                    : undefined
+            })),
+        }
+    }, { query: z.object({ roomId: z.string() }) })
 
 
 
 const app = new Elysia({ prefix: "/api" }).use(rooms).use(messages)
 
 
-export const GET = app.fetch 
-export const POST = app.fetch 
+export const GET = app.fetch
+export const POST = app.fetch
+export const DELETE = app.fetch
 
 export type App = typeof app
